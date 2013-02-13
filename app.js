232637e08def4818b4b6ac9b5144b9f31f2756d1
Alexandre Kaspar <xion.luhnis@gmail.com>
2013-02-13 17:16:00 +0900

    Use config.json (note: there's only the config.json.sample in git)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var assets = require('connect-assets');
+var config = require('./config.json');
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -9,20 +10,20 @@ var auth = require('./routes/auth.js');
 
 // config
 app.configure(function () {
-  app.set('port', 3000);
+  app.set('port', config.port);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'jade');
   // console logger
   app.use(express.logger('dev'));
-  app.use(express.favicon(path.join(app.util.publicDir, 'images', 'favicon.ico')));
+  app.use(express.favicon(path.join(config.publicDir, 'images', 'favicon.ico')));
   app.use(express.bodyParser());
   // using cookies and sessions for authentication
   app.use(express.cookieParser());
   app.use(express.session({
     key: 'sessionid',
-    secret: app.util.sessionSecret,
+    secret: config.session.ecret,
     cookie: {
-      path: app.util.prefix,
+      path: config.prefix,
       maxAge: 2 * 24 * 3600 * 1000
     },
     store: redis.getStore(express)
@@ -41,15 +42,15 @@ app.configure(function () {
   // routing from here
   app.use(app.router);
   // assets reduction
-  app.use(app.util.prefix, assets({
-    src: app.util.publicPath,
+  app.use(config.prefix, assets({
+    src: config.publicPath,
     helperContext: assets,
     buildDir: false,
-    servePath: app.util.prefix,
+    servePath: config.prefix,
     minifyBuilds: false
   }));
   // public directory
-  app.use(app.util.prefix, express['static'](app.util.publicDir));
+  app.use(config.prefix, express['static'](config.publicDir));
 });
 
 app.configure('development', function () {
@@ -59,34 +60,34 @@ app.configure('development', function () {
 // Routing
 var router = require('./routes/router.js');
 var rx = function(body){
-  return new RegExp('^' + app.util.prefix + body + '$');
+  return new RegExp('^' + config.prefix + body + '$');
 };
 
 // - base and auth
 app.get(rx('/?'), router.index);
-app.all(app.util.prefix + '/auth', auth.auth);
-app.all(app.util.prefix + '/auth/callback', auth.callback);
+app.all(config.prefix + '/auth', auth.auth);
+app.all(config.prefix + '/auth/callback', auth.callback);
 
 // - index structure
 var route_rest_index = require('./routes/rest-index.js');
-app.get(app.util.prefix + '/rest/index', route_rest_index.get);
-app.post(app.util.prefix + '/rest/index', route_rest_index.create);
-app.put(app.util.prefix + '/rest/index', route_rest_index.edit);
-app.del(app.util.prefix + '/rest/index', route_rest_index.clear);
+app.get(config.prefix + '/rest/index', route_rest_index.get);
+app.post(config.prefix + '/rest/index', route_rest_index.create);
+app.put(config.prefix + '/rest/index', route_rest_index.edit);
+app.del(config.prefix + '/rest/index', route_rest_index.clear);
 
 // - gallery content and processing
 var route_rest_gallery = require('./routes/rest-gallery.js');
-app.get(app.util.prefix + '/rest/gallery', route_rest_gallery.get);
-app.post(app.util.prefix + '/rest/gallery', route_rest_gallery.create);
-app.put(app.util.prefix + '/rest/gallery', route_rest_gallery.edit);
-app.del(app.util.prefix + '/rest/gallery', route_rest_gallery.clear);
+app.get(config.prefix + '/rest/gallery', route_rest_gallery.get);
+app.post(config.prefix + '/rest/gallery', route_rest_gallery.create);
+app.put(config.prefix + '/rest/gallery', route_rest_gallery.edit);
+app.del(config.prefix + '/rest/gallery', route_rest_gallery.clear);
 
 // - metadata content and processing
 var route_rest_meta = require('./routes/rest-metadata.js');
-app.get(app.util.prefix + '/rest/metadata', route_rest_meta.get);
-app.post(app.util.prefix + '/rest/metadata', route_rest_meta.create);
-app.put(app.util.prefix + '/rest/metadata', route_rest_meta.edit);
-app.del(app.util.prefix + '/rest/metadata', route_rest_meta.clear);
+app.get(config.prefix + '/rest/metadata', route_rest_meta.get);
+app.post(config.prefix + '/rest/metadata', route_rest_meta.create);
+app.put(config.prefix + '/rest/metadata', route_rest_meta.edit);
+app.del(config.prefix + '/rest/metadata', route_rest_meta.clear);
 
 // - general routing and jade fallback
 app.get(rx('(/([0-9a-zA-Z]+[0-9a-zA-Z_\\-\\.]*/)+)'), router.route);
